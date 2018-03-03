ebaad3ec796ca2ce4f3607a020332f86c50e6082
selectfromwhere <yuto.tanaka.biz@gmail.com>
2018-03-03 09:32:37 +0900

    Add Company Review List Script

---
+++ Co_Reviews/Login.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import scraping_info as s_inf
+
+browser = webdriver.Chrome(s_inf.path_biz)
+# browser = webdriver.Chrome(chromedriver.path_home)
+
+browser.get(s_inf.url)
+browser.find_element_by_name('_username').send_keys(s_inf.user_name)
+browser.find_element_by_name('_password').send_keys(s_inf.password)
+browser.find_element_by_id('log_in').click()
+
+# browser.get(s_inf.targets['y'])
